7778143bb60e7e6ea380838cba1f57c7884da134
Ikhsan Faruqi <faruqisan@gmail.com>
2020-03-15 12:05:01 +0700

    feat: about page mobile layout

--- js/kitapelosok.js
+++ js/kitapelosok.js
@@ -1,11 +1,26 @@
-(function($) {
+
+function changeNavColorTheme(theme) {
+  switch (theme) {
+    case 'light':
+      $(".navbar-brand-img").attr("src", "img/logos/kitapelosok-h-tp.png");
+      $(".nav-link").css('color', 'white');
+      $(".navbar-toggler").css('color', 'white');
+      break;
+    case 'dark':
+      $(".navbar-brand-img").attr("src", "img/logos/kitapelosok-h-color.png");
+      $(".nav-link").css('color', 'black');
+      $(".navbar-toggler").css('color', 'black');
+  }
+}
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
@@ -24,7 +39,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
+  $(".js-scroll-trigger").click(function () {
     $(".navbar-collapse").collapse("hide");
   });
 
@@ -38,7 +53,7 @@
 
   // check what navbar is used
   if ($("#mainNav").length) {
-    var navbarCollapse = function() {
+    var navbarCollapse = function () {
       if ($("#mainNav").offset().top > 100) {
         $("#mainNav").addClass("navbar-shrink");
         $("#mainNav").css("background-color", "#256a81");
@@ -52,17 +67,15 @@
     navbarCollapse();
     $(window).scroll(navbarCollapse);
   } else {
-    var whiteNavbarCollapse = function() {
+    var whiteNavbarCollapse = function () {
       if ($("#whiteNav").offset().top > 100) {
         $("#whiteNav").addClass("navbar-shrink");
         $("#whiteNav").css("background-color", "#256a81");
-        $(".navbar-brand-img").attr("src", "img/logos/kitapelosok-h-tp.png")
-        $(".nav-link").css('color', 'white')
+        changeNavColorTheme("light")
       } else {
         $("#whiteNav").removeClass("navbar-shrink");
         $("#whiteNav").css("background-color", "transparent");
-        $(".navbar-brand-img").attr("src", "img/logos/kitapelosok-h-color.png")
-        $(".nav-link").css('color', 'black')
+        changeNavColorTheme("dark")
       }
     };
 
@@ -73,22 +86,44 @@
   }
 })(jQuery); // End of use strict
 
+// for mobile toggler
+
 var togglerClicked = false;
 
-$("#toggler-btn").click(function() {
+$("#toggler-btn").click(function () {
   // if page on top only change the bg
-  if ($("#mainNav").offset().top < 100) {
-    // if it's not clicked before (bg should be transparent)
-    // set bg to solid colod
-    if (!togglerClicked) {
-      $("#mainNav").css("background-color", "#256a81");
-      togglerClicked = true;
-    } else {
-      // if btn already clicked (bg should be solid)
-      // and user click btn again to colapse the navbar
-      // set bg to transparent again
-      $("#mainNav").css("background-color", "transparent");
-      togglerClicked = false;
+
+  if ($("mainNav").length) {
+    if ($("#mainNav").offset().top < 100) {
+      // if it's not clicked before (bg should be transparent)
+      // set bg to solid colod
+      if (!togglerClicked) {
+        $("#mainNav").css("background-color", "#256a81");
+        togglerClicked = true;
+      } else {
+        // if btn already clicked (bg should be solid)
+        // and user click btn again to colapse the navbar
+        // set bg to transparent again
+        $("#mainNav").css("background-color", "transparent");
+        togglerClicked = false;
+      }
+    }
+  } else {
+    if ($("#whiteNav").offset().top < 100) {
+      // if it's not clicked before (bg should be transparent)
+      // set bg to solid colod
+      if (!togglerClicked) {
+        $("#whiteNav").css("background-color", "#256a81");
+        changeNavColorTheme("light")
+        togglerClicked = true;
+      } else {
+        // if btn already clicked (bg should be solid)
+        // and user click btn again to colapse the navbar
+        // set bg to transparent again
+        $("#whiteNav").css("background-color", "transparent");
+        changeNavColorTheme("dark")
+        togglerClicked = false;
+      }
     }
   }
 });
